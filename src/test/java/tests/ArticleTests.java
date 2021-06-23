bc36b1c514b5bbdfd904a58857fe36b18e22fa73
MaxHar <Harmikl57@gmail.com>
2021-06-23 17:33:19 +0300

    end of lessons

--- src/test/java/tests/ArticleTests.java
+++ src/test/java/tests/ArticleTests.java
@@ -1,9 +1,6 @@
 package tests;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Features;
-import io.qameta.allure.Step;
+import io.qameta.allure.*;
 import io.qameta.allure.junit4.DisplayName;
 import lib.CoreTestCase;
 import lib.ui.ArticlePageObject;
@@ -12,6 +9,7 @@ import lib.ui.factories.ArticlePageObjectFactory;
 import lib.ui.factories.SearchPageObjectFactory;
 import org.junit.Assert;
 import org.junit.Test;
+@Epic("Tests for articles")
 public class ArticleTests extends CoreTestCase
 {
     @Test
@@ -19,6 +17,7 @@ public class ArticleTests extends CoreTestCase
     @DisplayName("Compare article title with expected one")
     @Description("We open Java Object-oriented programming language article and make sure the title is expected")
     @Step("Starting test testCompareArticleTitle")
+    @Severity(value=SeverityLevel.BLOCKER)
     public void testCompareArticleTitle() throws InterruptedException {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
 
@@ -42,6 +41,8 @@ public class ArticleTests extends CoreTestCase
     @DisplayName("Swipe article to the footer")
     @Description("We open an article and swipe to footer")
     @Step("Starting test testSwipeArticle")
+    @Severity(value=SeverityLevel.MINOR)
+
     public void testSwipeArticle() throws InterruptedException {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
         SearchPageObject.initSearchInput();
